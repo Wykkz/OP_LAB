ee1d6343797b9babb95a6e15e16ab2896db38645
Wykkz <chikaldintimur@gmail.com>
2022-09-19 23:40:07 +0900

    git commit

---
+++ numerovan.cpp
@@ -0,0 +1,11 @@
+#include <iostream>;
+#include <math.h>;
+#include <stdio.h>;
+using namespace std;
+void main()
+    {
+        int a, b, s;
+        scanf(”%i%i”,&a,&b);
+        s=a+b;
+        printf(”s=%i”,s);
+    }
