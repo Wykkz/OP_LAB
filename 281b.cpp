ac7e76b6ad2bb03e0be098235f245d4b77da28ad
Wykkz <chikaldintimur@gmail.com>
2022-09-20 01:25:08 +0900

    commit

---
+++ 281b.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int a;
+    cout <<"Vvedite a: ";
+    cin>> a;
+    int aa= a * a;
+    int aaa= aa * aa;
+    int aaaa= aaa * aaa;
+    int aaaaa = aaaa * aa;
+    cout << "a^10: " << aaaaa << endl;
+}
---
+++ 281c.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int a;
+    cout <<"Vvedite a: ";
+    cin>> a;
+    int a1= a * a;
+    int a2= a1 * a1;
+    int a3= a2 * a2;
+    int a4= a3 * a2;
+    cout << "a^12: " << a4 << endl;
+}
